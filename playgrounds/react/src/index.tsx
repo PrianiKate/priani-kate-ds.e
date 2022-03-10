6c779c3608ac669b3e731dbedbcb3ee766ef1920
Ekaterina Prianishnikova <ekaterina-gd@joincalibrate.com>
2022-03-10 16:28:29 +0400

    feat: add font size component

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Color } from '@ds.e/react/lib';
+import { Color, Text } from '@ds.e/react';
+
 import '@ds.e/scss/lib/Utilities.css';
+import '@ds.e/scss/lib/Text.css';
+import '@ds.e/scss/lib/global.css';
 
 ReactDOM.render(
-  <Color hexCode='#000' />,
+  <>
+    <Text size="xl">
+      Test
+    </Text>
+    <Color hexCode='#000' />
+  </>,
   document.querySelector('#root')
 );
