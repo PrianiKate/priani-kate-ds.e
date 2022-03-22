5e6c27eb95be359448c429d0d3959629dcddfa4c
Ekaterina Prianishnikova <ekaterina-gd@joincalibrate.com>
2022-03-22 12:16:32 +0400

    feat: add margin component

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Color, Text } from '@ds.e/react';
+import { Color, Text, Margin } from '@ds.e/react';
 
 import '@ds.e/scss/lib/Utilities.css';
 import '@ds.e/scss/lib/Text.css';
+import '@ds.e/scss/lib/Margin.css';
 import '@ds.e/scss/lib/global.css';
 
 ReactDOM.render(
-  <>
-    <Text size="xl">
-      Test
-    </Text>
+  <div>
+    <Margin>
+      <Text size="xl">
+        Test
+      </Text>
+    </Margin>
     <Color hexCode='#000' />
-  </>,
+  </div>,
   document.querySelector('#root')
 );
